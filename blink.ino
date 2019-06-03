5a50e37973115028dc5a9fcec699bac6f139af3a
jnny <jenny@sudomesh.org>
2019-06-02 23:29:49 -0700

    updated script toward dimming

--- blink.ino
+++ blink.ino
@@ -6,14 +6,41 @@
  * ON Board LED GPIO 2
  */
  
-#define LED 2
+// #define LED 2
  
+#define LED 21
+
+int freq = 1000;
+int ledChannel = 0;
+int resolution = 8;
+
 void setup() {
-  // Set pin mode
-  pinMode(LED,OUTPUT);
+  ledcSetup(ledChannel, freq, resolution);
+  ledcAttachPin(LED, ledChannel);
+ 
 }
  
 void loop() {
+ 
+  for (int dutyCycle = 0; dutyCycle <= 255; dutyCycle++) {
+    ledcWrite(ledChannel, dutyCycle);
+    delay(7);
+  }
+ 
+  for (int dutyCycle = 255; dutyCycle >= 0; dutyCycle--) {
+    ledcWrite(ledChannel, dutyCycle);
+    delay(7);
+  }
+}
+ 
+/*
+void loop() {
+  delay(3000);
+  digitalWrite(LED, LOW);
+  delay(3000);
+  digitalWrite(LED, HIGH);
+*/
+/*
   int i;
   for(i=0; i<400; i++){
     if(i<200){
@@ -28,4 +55,5 @@ void loop() {
       digitalWrite(LED, HIGH);
     }
   }
-}
+*/
+
